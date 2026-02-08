1926745af0f4bac0c984f04869e8120cc9b56fc8
hariniselva2008-sketch <harini.selva2008@gmail.com>
2026-02-08 12:00:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+const song = document.getElementById("bgSong");
+
+function goToPage(pageNum) {
+
+  // Song start when user clicks first next button
+  if (pageNum === 2) {
+    song.play();
+  }
+
+  // Hide all pages
+  for (let i = 1; i <= 4; i++) {
+    document.getElementById("page" + i).classList.remove("active");
+  }
+
+  // Show selected page
+  document.getElementById("page" + pageNum).classList.add("active");
+}
+
+function stopMusic() {
+  song.pause();
+  song.currentTime = 0;
+  alert("I Love you 💖😘");
+}
+
+// Create moving dots
+function createDots(dotContainerId) {
+  const container = document.getElementById(dotContainerId);
+
+  for (let i = 0; i < 40; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+
+    dot.style.left = Math.random() * 100 + "vw";
+    dot.style.animationDuration = (3 + Math.random() * 5) + "s";
+    dot.style.opacity = Math.random();
+
+    container.appendChild(dot);
+  }
+}
+
+createDots("dots1");
+createDots("dots2");
+createDots("dots3");
+createDots("dots4");
